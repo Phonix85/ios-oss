9a252422bc49539fe55d27b9cfc15a39551d984f
Phonix85 <euan_carmichael@hotmail.com>
2017-10-13 18:19:47 -0700

    added opentok to pre-build script

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,3 +1,7 @@
 if [ ! -d Frameworks/native-secrets ]; then
   mkdir -p Frameworks/native-secrets/ios && cp -n Configs/Secrets.swift.example Frameworks/native-secrets/ios/Secrets.swift
 fi
+if [ ! -d Frameworks/OpenTok ]; then
+  curl -s -N -L https://tokbox.com/downloads/opentok-ios-sdk-2.10.2 \
+		| tar -xz --strip 1 --directory Frameworks/OpenTok OpenTok-iOS-2.10.2/OpenTok.framework
+fi
