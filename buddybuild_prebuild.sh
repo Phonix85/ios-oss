d315c4fb880de411dd7d281a80a8ac3d613d930c
Phonix85 <euan_carmichael@hotmail.com>
2017-10-15 21:36:14 -0700

    invoke makefile from pre-build script

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,8 +1,1 @@
-if [ ! -d Frameworks/native-secrets ]; then
-  mkdir -p Frameworks/native-secrets/ios && cp -n Configs/Secrets.swift.example Frameworks/native-secrets/ios/Secrets.swift
-fi
-if [ ! -d Frameworks/OpenTok ]; then
-  mkdir -p Frameworks/OpenTok;
-  curl -s -N -L https://tokbox.com/downloads/opentok-ios-sdk-2.10.2 \
-		| tar -xz --strip 1 --directory Frameworks/OpenTok OpenTok-iOS-2.10.2/OpenTok.framework
-fi
+make -f Makefile
